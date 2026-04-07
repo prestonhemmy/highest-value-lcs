5dee5c361dcfaa96e0c58bba86a25f403baf942a
prestonhemmy <phemmy@ufl.edu>
2026-04-07 17:08:27 -0400

    feat: implement dp table for optimal value computation

--- src/hvlcs.py
+++ src/hvlcs.py
@@ -2,12 +2,32 @@ from itertools import islice
 import sys
 
 
+# TODO
+def hvlcs(a, b, values):
+    m = len(a)
+    n = len(b)
+
+    # initialize 0th row and 0th col to all 0s
+    table = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if a[i - 1] == b[j - 1]:
+                # add value if matching
+                table[i][j] = max(values[a[i - 1]] + table[i - 1][j - 1], table[i - 1][j], table[i][j - 1])
+            else:
+                # o.w. recurse
+                table[i][j] = max(table[i - 1][j], table[i][j - 1])
+
+    return table[m][n]
+
 # Usage: python hvlcs.py <input-file>
 def main():
     if len(sys.argv) != 2:
         print("Usage: python hvlcs.py <input-file>")
         sys.exit(1)
 
+    # parse input file
     filename = sys.argv[1]
 
     with open(filename, 'r') as file:
@@ -15,17 +35,19 @@ def main():
 
         # character values dict
         values = {
-            line.split()[0]: line.split()[1]
+            line.split()[0]: int(line.split()[1])
             for line in islice(file, n)
         }
 
         # input strings
-        A = file.readline().strip()
-        B = file.readline().strip()
+        a = file.readline().strip()
+        b = file.readline().strip()
 
-    # TODO: call hvlcs()
+    # invoke hvlcs()
+    max_val = hvlcs(a, b, values)
+    print(max_val)
 
-    # TODO: call backtrack()
+    # TODO: invoke backtrack()
 
     # TODO: file write
 
